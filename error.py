1e643b6c4d48278e9c9e725c50412f015b5feb60
CJS26 <chrsan26@bergen.org>
2024-03-12 09:04:28 -0400

    done

--- error.py
+++ error.py
@@ -1,1 +1,19 @@
+import json
+import csv
+import re
 
+json_file = "logs.json"
+csv_file = "error_logs.csv"
+
+with open(json_file,'r')as file:
+    data = json.load(file)
+    with open(csv_file,'w',newline='')as file2:
+        writer = csv.writer(file2)
+        writer.writerow(['timestamp', 'level', 'message'])
+        for entry in data:
+            pattern = r"\w(ERROR)"
+            print(entry["level"])
+            print(re.findall(entry["level"],pattern,re.IGNORECASE))
+            if re.findall(entry["level"],pattern):
+                entry_data = [entry['timestamp'],entry['level'],entry['message']]
+                writer.writerow(entry_data)
